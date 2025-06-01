e2997179bb4fa6981d84a2f88b235f246d27f9e7
Robin <robin@simplitics.se>
2025-06-01 20:09:05 +0200

    Misc

---
+++ spirida-python/docs/conf.py
@@ -0,0 +1,12 @@
+project = 'Spirida'
+copyright = '2025, Robin Langell'
+author = 'Robin Langell'
+release = '0.1'
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ spirida-python/docs/modules.rst
@@ -0,0 +1,10 @@
+Spirida Module Reference
+========================
+
+.. automodule:: spirida.core
+   :members:
+   :undoc-members:
+
+.. automodule:: spirida.spiralbase
+   :members:
+   :undoc-members:
